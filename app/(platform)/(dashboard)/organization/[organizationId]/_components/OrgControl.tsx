cfce35f4b662c39dd6abb1dbb1a1e26fc13bd85f
Duy ND <duynd1006@gmail.com>
2023-12-09 09:49:50 +0700

    finished organization ID page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/OrgControl.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useOrganizationList } from "@clerk/nextjs";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+
+const OrgControl = () => {
+  const params = useParams();
+  const { setActive } = useOrganizationList();
+
+  useEffect(() => {
+    if (!setActive) return;
+
+    setActive({
+      organization: params.organizationId as string,
+    });
+  }, [setActive, params.organizationId]);
+
+  return (
+    <div>OrgControl</div>
+  );
+};
+
+export default OrgControl;
