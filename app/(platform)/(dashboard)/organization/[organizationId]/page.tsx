d7bd5ca716c6fc9c5a847467b5eb7f180ab0f9b4
Duy ND <duynd1006@gmail.com>
2023-12-13 23:02:45 +0700

    finished implementing delete board server action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,30 @@
+import { create } from "@/actions/createBoard";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { Board } from "./Board";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
   return (
-    <div>
-      Organization ID Page
+    <div className="flex flex-col space-y-4">
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+        <Button type="submit">
+          Submit
+        </Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
     </div>
   );
 };
