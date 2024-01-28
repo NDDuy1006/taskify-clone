4469724ddf86d292fac538f3ae7350ac361e22b2
Duy ND <duynd1006@gmail.com>
2024-01-28 11:29:12 +0700

    form adjustment

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,19 +1,12 @@
-import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
-import { Board } from "./Board";
-import { Form } from "./Form";
+import Info from "./_components/Info";
 
 const OrganizationIdPage = async () => {
   const boards = await db.board.findMany();
 
   return (
-    <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id} />
-        ))}
-      </div>
+    <div className="w-full mb-20">
+      <Info />
     </div>
   );
 };
