fbadb20afd63c48bb3ab1c50c51659392b10a7d4
Duy ND <duynd1006@gmail.com>
2024-02-19 22:47:03 +0700

    finished form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "@/lib/db";
 import Info from "./_components/Info";
+import { Separator } from "@/components/ui/separator";
+import BoardList from "./_components/BoardList";
 
 const OrganizationIdPage = async () => {
   const boards = await db.board.findMany();
@@ -7,6 +9,10 @@ const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
---
+++ components/Hint.tsx
@@ -0,0 +1,39 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/components/ui/tooltip";
+
+interface IProps {
+  children: React.ReactNode;
+  description: string;
+  side?: "left" | "right" | "top" | "bottom";
+  sideOffset?: number;
+}
+
+const Hint = ({
+  children,
+  description,
+  side = "bottom",
+  sideOffset
+}: IProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          {children}
+        </TooltipTrigger>
+        <TooltipContent
+          sideOffset={sideOffset}
+          side={side}
+          className="text-xs max-w-[220px] break-words"
+        >
+          {description}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default Hint;
